ec3facf46de11007e9ff02aa235c6a113ae47334
Siddhant Chakraborty <121742181+Sid797@users.noreply.github.com>
2024-07-15 17:43:21 +0000

    added 4 more questions

--- day-1/index.js
+++ day-1/index.js
@@ -9,3 +9,56 @@ var findDuplicate = function(nums) {
     }
     return -1; 
 };
+// sort colors-75
+var sortColors = function(nums) {
+   nums.sort(); 
+};
+// remove duplicates from sorted array
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0;
+
+    let k = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
+// move zeroes
+var moveZeroes = function(nums) {
+    let lastNonZeroFoundAt = 0;
+
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[lastNonZeroFoundAt] = nums[i];
+            lastNonZeroFoundAt++;
+        }
+    }
+
+    for (let i = lastNonZeroFoundAt; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+
+    return nums;
+};
+// best time to buy and sell stock-121
+const maxProfit = (prices) => {
+  let left = 0;
+  let right = 1; 
+  let max_profit = 0;
+
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      let profit = prices[right] - prices[left]; 
+      max_profit = Math.max(max_profit, profit);
+    } else {
+      left = right;
+    }
+    right++;
+  }
+  return max_profit;
+}
